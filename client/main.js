bca7f58ac0052beaebbc8d5233b48112fbd15db4
jiteshrai <jiteshrai.ostech@gmail.com>
2016-07-07 11:10:53 +0530

    My fisrt commit

---
+++ client/main.js
@@ -0,0 +1,56 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Resolutions = new Meteor.Collection('resolutions');
+console.log(Resolutions);
+if( Meteor.isClient ){
+
+	Template.body.helpers({
+		
+		resolutions: function(){
+			console.log(Resolutions.find());
+			if( Session.get('hideFinished')){
+				return Resolutions.find({checked: {$ne: true}});
+			}else{
+				 return Resolutions.find();
+			}
+			
+		},
+		hideFinished: function(){
+			return Session.get('hideFinished');
+		}
+	});
+
+	Template.body.events({
+		'submit .new-resolution': function(event){
+			var title = event.target.title.value;
+
+			Resolutions.insert({
+				title: title,
+				createdAt: new Date()
+			});
+
+			event.target.title.value = "";
+
+			return false;
+		},
+		'change .hide-finished': function(event){
+			Session.set('hideFinished', event.target.checked );
+		}
+	});
+
+	Template.resolution.events({
+		'click .toggle-checked': function(){
+			Resolutions.update(this._id, {$set: {checked: !this.checked}});
+		},
+		'click .delete': function(){
+			Resolutions.remove(this._id);
+		}
+	});
+
+	Accounts.ui.config({
+		passwordSignupFields: "USERNAME_ONLY"
+	});
+}
---
+++ server/main.js
@@ -0,0 +1,5 @@
+import { Meteor } from 'meteor/meteor';
+Resolutions = new Meteor.Collection('resolutions');
+Meteor.startup(() => {
+  // code to run on server at startup
+});
